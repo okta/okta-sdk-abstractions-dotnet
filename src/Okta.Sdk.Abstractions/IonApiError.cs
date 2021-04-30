0831f6cf8828df72f58959af347a9f0b80304c8a
Andrii Zhegurov <68990515+andriizhegurov-okta@users.noreply.github.com>
2021-04-30 23:37:23 +0300

    OKTA-389639 [.NET] Handle local form field level errors from IDXResponse (#12)

--- src/Okta.Sdk.Abstractions/IonApiError.cs
+++ src/Okta.Sdk.Abstractions/IonApiError.cs
@@ -5,7 +5,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace Okta.Sdk.Abstractions
 {
@@ -19,6 +21,18 @@ namespace Okta.Sdk.Abstractions
         public string ErrorSummary => GetErrorSummary();
 
         private string GetErrorSummary()
+        {
+            var errorMessage = GetResponseErrorSummary();
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                return errorMessage;
+            }
+
+            return GetFormValidationErrorSummary();
+        }
+
+        // Extracts error message from the top level of response object
+        private string GetResponseErrorSummary()
         {
             var sbErrorSumary = new StringBuilder();
             var messageObj = this.GetProperty<BaseResource>("messages");
@@ -37,6 +51,78 @@ namespace Okta.Sdk.Abstractions
             }
 
             return sbErrorSumary.ToString();
+        }
+
+        /*
+         Extracts form validation error messages like in this case:
+
+         {
+          "version": "1.0.0",
+          "stateHandle": "028e-r0fW99Ud4dADthU5wogZhdxzLhvOUPnC6FXVx",
+          "expiresAt": "2021-04-27T22:05:50.000Z",
+          "intent": "LOGIN",
+          "remediation": {
+            "type": "array",
+            "value": [
+              {
+                "rel": [
+                  "create-form"
+                ],
+                "name": "reset-authenticator",
+                "relatesTo": [
+                  "$.currentAuthenticator"
+                ],
+                "href": "..................",
+                "method": "POST",
+                "produces": "application/ion+json; okta-version=1.0.0",
+                "value": [
+                  {
+                    "name": "credentials",
+                    "type": "object",
+                    "form": {
+                      "value": [
+                        {
+                          "name": "passcode",
+                          "label": "New password",
+                          "secret": true,
+                          "messages": {
+                            "type": "array",
+                            "value": [
+                              {
+                                "message": "Password requirements were not met. Password requirements: at least 8 characters, a lowercase letter, an uppercase letter, a number, no parts of your username. Your password cannot be any of your last 4 passwords.",
+                                "i18n": {
+                                  "key": "password.passwordRequirementsNotMet",
+                                  "params": [
+                                    "Password requirements: at least 8 characters, a lowercase letter, an uppercase letter, a number, no parts of your username. Your password cannot be any of your last 4 passwords."
+                                  ]
+            },
+                                "class": "ERROR"
+                              }
+                            ]
+                          }
+                        }
+                      ]
+                    },
+                    "required": true
+                  },
+                  {
+        ........
+                }
+                ],
+                "accepts": "application/json; okta-version=1.0.0"
+              }
+            ]
+          },
+ .........
+        }
+        */
+        private string GetFormValidationErrorSummary()
+        {
+            var jToken = JToken.Parse(GetRaw());
+            var errorMessages = jToken
+                                    .SelectTokens("$.remediation.value[*].value[*].form.value[*].messages.value[*].message")
+                                    .Select(t => t.ToString());
+            return string.Join(Environment.NewLine, errorMessages);
         }
     }
 }
