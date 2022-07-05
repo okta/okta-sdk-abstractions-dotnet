014ff2140a68b5c6fb0db082059eecc0f1e7b297
Bryan Apellanes <bryan.apellanes@okta.com>
2022-07-05 13:48:22 -0500

    OKTA-506997: set default authorization header if token is specified in configuration

---
+++ src/Okta.Sdk.Abstractions.UnitTests/Internal/DefaultRequestExecutorShould.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Okta.Sdk.Abstractions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace Okta.Sdk.Abstractions.UnitTests.Internal
+{
+    public class DefaultRequestExecutorShould
+    {
+        [Fact]
+        public void SetHttpClientDefaultRequestAuthorizationHeader()
+        {
+            var testToken = "abcd1234-fake-token";
+            var httpClient = new HttpClient();
+            var oktaConfig = new OktaClientConfiguration { Token = testToken, OktaDomain = "https://test.okta.com" };
+            _ = new DefaultRequestExecutor(oktaConfig, httpClient, null);
+
+            httpClient.DefaultRequestHeaders.Should().NotBeNull();
+            httpClient.DefaultRequestHeaders.Authorization.Should().NotBeNull();
+            httpClient.DefaultRequestHeaders.Authorization.Scheme.Should().Be("SSWS");
+            httpClient.DefaultRequestHeaders.Authorization.Parameter.Should().Be(testToken);
+        }
+    }
+}
